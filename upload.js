c932760760acaa0b732d51216973d69566fe728b
Nathan <martinellinathan939@gmail.com>
2020-04-21 15:56:17 +0200

    Code fonctionnel d'envoi au serveur

---
+++ upload.js
@@ -0,0 +1,10 @@
+var commande = new FormData();
+commande.append("couleur", "rouge");
+commande.append("pointure", "43");
+// Envoi de l'objet FormData au serveur
+ajaxPost("http://localhost:8090/DeepCodeurPHP/post_form.php", commande,
+    function (reponse) {
+        // Affichage dans la console en cas de succès
+        console.log("Commande envoyée au serveur");
+    }
+);
