608245b790f9e3808590d3b286e57ff8470d9e06
Nathan <martinellinathan939@gmail.com>
2020-04-23 15:36:33 +0200

    Fonction parfaitement fonctionnel sans design et convertsseur audio

--- upload.js
+++ upload.js
@@ -8,3 +8,15 @@ ajaxPost("http://localhost:8090/DeepCodeurPHP/post_form.php", commande,
         console.log("Commande envoyée au serveur");
     }
 );
+var form = document.querySelector("form");
+// Gestion de la soumission du formulaire
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // Récupération des champs du formulaire dans l'objet FormData
+    var data = new FormData(form);
+    // Envoi des données du formulaire au serveur
+    // La fonction callback est ici vide
+    ajaxPost("http://localhost:8090/DeepCodeurPHP/post_form.php", data, function () {});
+});
+
+
