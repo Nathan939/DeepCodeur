a8ab263fd736d85ec2712a8566150adea6a37d88
Nathan <martinellinathan939@gmail.com>
2020-04-29 19:54:02 +0200

    Début de l'ajout de l'html aux fichiers Django

--- deepcodeurDJANGO/settings.py
+++ deepcodeurDJANGO/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'deepcodeurDJANGO.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
