642d28aa36a2fa7b4cfa22c678e63c527c59a8bb
Nathan <martinellinathan939@gmail.com>
2020-05-05 11:42:34 +0200

    Première version fonctionnel de django

--- deepcodeurDJANGO/urls.py
+++ deepcodeurDJANGO/urls.py
@@ -10,14 +10,16 @@ Function views
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    from django.urls import path
+]
+
+from django.urls import path
 from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accueil', views.home),
-]
 ]
