3ab8b501a38fa9131a4601cb5b68e5b915159f4b
Nathan <martinellinathan939@gmail.com>
2020-05-06 18:54:23 +0200

    javascript integration to django

--- deepcodeurDJANGO/settings.py
+++ deepcodeurDJANGO/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'   # Qui devrait déjà être la configuration par défaut
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
