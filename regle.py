eec9ac33b9536741074b6006946420d18887bda4
Nathan <martinellinathan939@gmail.com>
2020-05-13 22:01:16 +0200

    addition of development rules

---
+++ regle.py
@@ -0,0 +1,74 @@
+''' Module contenant les conventions de développement partie HTML '''
+
+nom = nom
+<head> = True
+<body> = True
+<head> = True
+<footer> = True
+
+def HTML():
+    def DOCTYPE ():
+        DOCTYPE = True
+        print:("<!DOCTYPE html>")
+
+        if DOCTYPE = True:
+            print("<html>/n</html>")
+            print("<head>/n</head>/n<body>/n</body>/n<head>/n</head>/n<footer>/n</footer>") 
+            in <head> print('<link rel="stylesheet" href="'+nomn+'">')
+
+        else:
+            print("erreur")
+    
+
+    def balise1():
+        balise1 = True
+
+        if balise1 = True:
+            print(balise1 + /n /n)
+        else:
+            print("erreur")
+        
+        balise1 = "<"+balise+">"
+
+    def balise2():
+        balise2 = True
+
+        if balise2 = True:
+            print(balise2 + /n /n)
+        else:
+            print("erreur")
+        
+        balise2 = "</"+balise+">"
+
+    def argument():
+        argument = True
+
+        if argument = True:
+            print("<"+ balise + argument +">")
+        else:
+            print("erreur")
+
+    def bib():
+        if balise in balise = True:
+            print(balise + /n + /t + balise)
+        else:
+            print("erreur")
+
+def CSS(., #):
+nomdeclass = nomdeclass
+nomdeid = nomdeid
+    def selection():
+        if . = True:
+            print(. + nomdeclass)
+
+        elif # = True:
+            print(# + nomdeid)
+
+        else:
+            print(balise)
+
+    def contenu():
+        if selection = True:
+            print(selection + "{" + /n + "}") 
+
+    
