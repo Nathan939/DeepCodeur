87ed87ae89c60f75ce983b9277d453e167bed1f9
Nathan <martinellinathan939@gmail.com>
2020-05-14 18:47:50 +0200

    Debug of rules

--- regle.py
+++ regle.py
@@ -1,20 +1,21 @@
 ''' Module contenant les conventions de développement partie HTML '''
 
-nom = nom
-<head> = True
-<body> = True
-<head> = True
-<footer> = True
+nom = True
+head = True
+body = True
+head = True
+footer = True
+balise = True
+
 
 def HTML():
     def DOCTYPE ():
         DOCTYPE = True
         print:("<!DOCTYPE html>")
 
-        if DOCTYPE = True:
-            print("<html>/n</html>")
-            print("<head>/n</head>/n<body>/n</body>/n<head>/n</head>/n<footer>/n</footer>") 
-            in <head> print('<link rel="stylesheet" href="'+nomn+'">')
+        if DOCTYPE == True:
+            print("<html>\n</html>")
+            print('<head>\n</head>'+'\n<body>\n</body>'+'\n<head>'+'\n<link rel="stylesheet" href="'+'str(nom)"'+'>\n</head>\n<footer>\n</footer>') 
 
         else:
             print("erreur")
@@ -23,18 +24,18 @@ def HTML():
     def balise1():
         balise1 = True
 
-        if balise1 = True:
-            print(balise1 + /n /n)
+        if balise1 == True:
+            print(str(balise1) + "\n \n")
         else:
             print("erreur")
         
-        balise1 = "<"+balise+">"
+        balise1 = "<"+balise+">"\
 
     def balise2():
         balise2 = True
 
-        if balise2 = True:
-            print(balise2 + /n /n)
+        if balise2 == True:
+            print(str(balise2) + "\n \n")
         else:
             print("erreur")
         
@@ -43,32 +44,34 @@ def HTML():
     def argument():
         argument = True
 
-        if argument = True:
+        if argument == True:
             print("<"+ balise + argument +">")
         else:
             print("erreur")
 
     def bib():
-        if balise in balise = True:
-            print(balise + /n + /t + balise)
+        if balise in balise == True:
+            print(balise + "/n" + "/t" + balise)
         else:
             print("erreur")
 
-def CSS(., #):
-nomdeclass = nomdeclass
-nomdeid = nomdeid
+def CSS():
+    ndc = ndc
+    point = point
+    dièse = dièse
+    nomdeid = nomdeid
     def selection():
-        if . = True:
-            print(. + nomdeclass)
+            if "." == True:
+                print(str(point) + str(ndc))
 
-        elif # = True:
-            print(# + nomdeid)
+            elif dièse == True:
+                print(dièse + str(nomdeid))
 
-        else:
-            print(balise)
+            else:
+                print(str(balise1))
 
     def contenu():
-        if selection = True:
-            print(selection + "{" + /n + "}") 
+        if selection == True:
+            print(str(selection) + "{" + "\n" + "}") 
 
     
