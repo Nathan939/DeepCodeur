39c47d120578bc48e3cd22933738a028a1ca8aa8
Etienne <etienne@le-gallic.com>
2020-05-20 11:42:49 +0200

    Auto Scraping V3

--- _Scraping/AutoScrap.py
+++ _Scraping/AutoScrap.py
@@ -19,7 +19,7 @@ if response.ok:
     soup = BeautifulSoup(response.text, 'lxml')
     links = soup.findAll('a')
     for link in links:
-        if len(link.get('href')) > 2 and link.get('href')[1] == 'w':
+        if len(link.get('href')) > 2 and link.get('href')[1] == 'w' and link.get('href') not in URLs:
             URLs.append(link.get('href'))
 
 ydl_opts = {
@@ -33,10 +33,15 @@ ydl_opts = {
 }
 
 while time.time() < timeout_start + timeout:
+    if len(URLs) == 0:
+        break
+
     N = str(choice(URLs))
+    URLs.remove(N)
+
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(['https://www.youtube.com' + N])
     print('\nDownloaded :', 'https://www.youtube.com' + N, '\n')
-
+    
     time.sleep(3)
     
