5207d515c4f349d20f707c990f87e72c21ec3508
Etienne <etienne@le-gallic.com>
2020-05-20 10:45:53 +0200

    Auto Scraping V2

--- _Scraping/AutoScrap.py
+++ _Scraping/AutoScrap.py
@@ -8,32 +8,35 @@ import youtube_dl
 url = 'https://www.youtube.com/'
 response = requests.get(url)
 
-timeout = 3600  
+timeout = 60
 timeout_start = time.time()
+URLs = []
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
+# while time.time() < timeout_start + timeout:
+if response.ok:
+    soup = BeautifulSoup(response.text, 'lxml')
+    links = soup.findAll('a')
+    for link in links:
+        if len(link.get('href')) > 2 and link.get('href')[1] == 'w':
+            URLs.append(link.get('href'))
+
+ydl_opts = {
+'format': 'bestaudio/best',
+'outtmpl': SITE_ROOT + '/Audios/%(title)s.%(ext)s',
+'postprocessors': [{
+    'key': 'FFmpegExtractAudio',
+    'preferredcodec': 'wav',
+    'preferredquality': '192',
+}],
+}
+
 while time.time() < timeout_start + timeout:
-    if response.ok:
-        soup = BeautifulSoup(response.text)
-        URL = soup.find('a')
-    URL = str(URL)
-    liste = [str(URL)]
-    N = choice(liste)
-
-    ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': SITE_ROOT + '/Audios/%(title)s.%(ext)s',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'wav',
-        'preferredquality': '192',
-    }],
-    }
-
+    N = str(choice(URLs))
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([N])
+        ydl.download(['https://www.youtube.com' + N])
+    print('\nDownloaded :', 'https://www.youtube.com' + N, '\n')
 
     time.sleep(3)
-
     
